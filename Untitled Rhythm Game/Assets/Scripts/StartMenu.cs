8ba77e7936908ca6f00c1066ddfa4b6797deb46f
ConstructorLeo <leobmlopez@gmail.com>
2021-09-16 11:20:10 +1000

    Start Menu Scripts

---
+++ Untitled Rhythm Game/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Establish game objects so they can be set in the inspector
+    public GameObject playGame = null;
+    public GameObject exit = null;
+
+    void Start()
+    {
+        // Establish each game object in the code
+        playGame = GameObject.Find("PlayGame");
+        exit = GameObject.Find("Exit");
+    }
+
+    // Goes to the first level (scene)
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("LEVEL_001");
+        Debug.Log("Pressed Start Button");
+    }
+
+    // Exits the game
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Pressed Quit Button");
+    }
+}
